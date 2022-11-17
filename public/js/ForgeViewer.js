dedbbfc5666754253c69d75fdec80bafb4e8c4c4
deepalics0044 <deepalics0044@gmail.com>
2022-11-17 17:01:43 +0530

    update

---
+++ public/js/ForgeViewer.js
@@ -0,0 +1,124 @@
+﻿/////////////////////////////////////////////////////////////////////
+// Copyright (c) Autodesk, Inc. All rights reserved
+// Written by Forge Partner Development
+//
+// Permission to use, copy, modify, and distribute this software in
+// object code form for any purpose and without fee is hereby granted,
+// provided that the above copyright notice appears in all copies and
+// that both that copyright notice and the limited warranty and
+// restricted rights notice below appear in all supporting
+// documentation.
+//
+// AUTODESK PROVIDES THIS PROGRAM "AS IS" AND WITH ALL FAULTS.
+// AUTODESK SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTY OF
+// MERCHANTABILITY OR FITNESS FOR A PARTICULAR USE.  AUTODESK, INC.
+// DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
+// UNINTERRUPTED OR ERROR FREE.
+/////////////////////////////////////////////////////////////////////
+
+var viewer;
+
+function launchViewer(urn) {
+  var options = {
+    env: "AutodeskProduction2",
+
+    getAccessToken: getForgeToken,
+  };
+
+  Autodesk.Viewing.Initializer(options, () => {
+    viewer = new Autodesk.Viewing.GuiViewer3D(
+      document.getElementById("forgeViewer"),
+      {
+        extensions: [
+          "MyAwesomeExtension",
+          "HandleSelectionExtension",
+          "ModelSummaryExtension",
+        ],
+      }
+    );
+
+    viewer.start();
+    viewer.loadExtension("Autodesk.PDF").then(() => {
+      viewer.loadModel("/ visualization_-_aerial.pdf");
+    });
+    var documentId = "urn:" + urn;
+    Autodesk.Viewing.Document.load(
+      documentId,
+      onDocumentLoadSuccess,
+      onDocumentLoadFailure
+    );
+  });
+}
+
+function onDocumentLoadSuccess(doc) {
+  var viewables = doc.getRoot().getDefaultGeometry();
+  viewer.loadDocumentNode(doc, viewables).then((i) => {
+    viewer.addEventListener(
+      Autodesk.Viewing.SELECTION_CHANGED_EVENT,
+
+      async function () {
+        const resp = await fetch("/sensors");
+        if (!resp.ok) {
+          throw new Error(await resp.text());
+        }
+        let data1 = await resp.json();
+
+        var dbID;
+        var red = new THREE.Vector4(1, 0, 0, 1);
+        var green = new THREE.Vector4(0, 128, 0, 1);
+
+        var myDbid = viewer.getSelection();
+        viewer.getProperties(myDbid, function (e) {
+          console.log("Entire object response ", e);
+          dbID = e;
+          console.log("Properties ", e.properties.label);
+        });
+
+        data1.forEach((e) => {
+          if (e.closed == true && e.locked == false) {
+            viewer.setThemingColor(e.dbid, red);
+          } else if (e.closed == false && e.locked == true) {
+            viewer.setThemingColor(e.dbid, green);
+            viewer.loadExtension("IconMarkupExtension", {
+              button: {
+                icon: "fa-door-closed",
+                tooltip: "Show locked door",
+              },
+              icons: [
+                {
+                  dbId: e.dbid,
+                  label: "Locked",
+                  css: "fas fa-solid fa-lock",
+                },
+              ],
+              onClick: (id) => {
+                viewers.select(id);
+                viewers.utilities.fitToView();
+                switch (id) {
+                  case 563:
+                    alert("Sensor offline");
+                }
+              },
+            });
+          } else if (e.closed == false && e.locked == false) {
+            viewer.setThemingColor(e.dbid, green);
+          } else {
+            viewer.setThemingColor(e.dbid, red);
+          }
+        });
+      }
+    );
+  });
+}
+
+function onDocumentLoadFailure(viewerErrorCode) {
+  console.error("onDocumentLoadFailure() - errorCode:" + viewerErrorCode);
+}
+
+function getForgeToken(callback) {
+  fetch("/api/forge/oauth/token").then((res) => {
+    res.json().then((data) => {
+      callback(data.access_token, data.expires_in);
+    });
+  });
+}
---
+++ start.js
@@ -0,0 +1,65 @@
+/////////////////////////////////////////////////////////////////////
+// Copyright (c) Autodesk, Inc. All rights reserved
+// Written by Forge Partner Development
+//
+// Permission to use, copy, modify, and distribute this software in
+// object code form for any purpose and without fee is hereby granted,
+// provided that the above copyright notice appears in all copies and
+// that both that copyright notice and the limited warranty and
+// restricted rights notice below appear in all supporting
+// documentation.
+//
+// AUTODESK PROVIDES THIS PROGRAM "AS IS" AND WITH ALL FAULTS.
+// AUTODESK SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTY OF
+// MERCHANTABILITY OR FITNESS FOR A PARTICULAR USE.  AUTODESK, INC.
+// DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
+// UNINTERRUPTED OR ERROR FREE.
+/////////////////////////////////////////////////////////////////////
+let data = [
+  { dbid: 2448, closed: false, locked: false },
+  { dbid: 2447, closed: false, locked: false },
+  { dbid: 2442, closed: false, locked: false },
+  { dbid: 2470, closed: false, locked: false },
+];
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 3000;
+const config = require("./config");
+if (
+  config.credentials.client_id == null ||
+  config.credentials.client_secret == null
+) {
+  console.error(
+    "Missing FORGE_CLIENT_ID or FORGE_CLIENT_SECRET env. variables."
+  );
+  return;
+}
+
+let app = express();
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json({ limit: "50mb" }));
+// Process application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+// Process application/json
+app.use(bodyParser.json());
+app.use("/api/forge/oauth", require("./routes/oauth"));
+app.use("/api/forge/oss", require("./routes/oss"));
+app.use("/api/forge/modelderivative", require("./routes/modelderivative"));
+app.post("/sensors", function (req, res) {
+  data = req.body;
+  res.status(200).end();
+  console.log(req.body);
+});
+app.get("/sensors", function (req, res) {
+  res.json(data);
+  console.log(req.body);
+});
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.statusCode).json(err);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
