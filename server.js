9f7a17ecd6d5a80e3a4fd473604027e9a83d190a
deepalics0044 <deepalics0044@gmail.com>
2022-12-06 23:37:41 +0530

    update

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const PORT = process.env.PORT || 3000;
+
+let app = express();
+app.use(express.static('public'));
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}...`));
